fb71e68c74d86e5ae0d769ff1ac4f9fc4be0e241
17trupti <167959684+17trupti@users.noreply.github.com>
2024-10-15 12:47:58 +0530

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace paradigm_shift_csharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Running tests...");
+            TestRunner.RunTests();
+        }
+    }
+}
